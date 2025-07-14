ba9424759b54d9beaea6eb4eb0126429ff717943
KaylynTV39 <kaylynvannorstrand@navapbc.com>
2025-07-14 16:47:35 -0400

    vercel test

---
+++ vercel-beta-5-ai/src/db/index.ts
@@ -0,0 +1,13 @@
+import * as schema from './schema'
+
+import { drizzle as drizzleNeon } from 'drizzle-orm/neon-http'
+import { drizzle as drizzlePostgres } from 'drizzle-orm/node-postgres'
+import { neon } from '@neondatabase/serverless'
+
+export const db = process.env.VERCEL
+    ? drizzleNeon({
+        client: neon(process.env.DATABASE_URL!),
+        schema,
+        casing: 'snake_case',
+    })
+    : drizzlePostgres(process.env.DATABASE_URL!, { schema, casing: 'snake_case' })
